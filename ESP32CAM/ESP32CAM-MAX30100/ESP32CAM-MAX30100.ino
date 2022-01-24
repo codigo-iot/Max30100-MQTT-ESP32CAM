fb86cb1076fa5ca5fa99262b283a717a5f3dbe0a
“hugoescalpelo” <hugoescalpelo@gmail.com>
2022-01-24 14:55:55 -0600

    Ejemplos agregados

---
+++ ESP32CAM/ESP32CAM-MAX30100/ESP32CAM-MAX30100.ino
@@ -0,0 +1,293 @@
+/*
+ * Envio de datos del sensor MAX30100 por MQTT
+ * por: Hugo Escalpelo
+ * Fecha: 24 de enero de 2022
+ * 
+ * Este programa lee el sensor I2C Max30100 y envía los valores de 
+ * ritmo cardiaco (HR) y oxigenación (SpO2) por MQTT. Toma la bilbioteca
+ * MAX30105 del Sparkfun.
+ * 
+ * Componente     PinESP32CAM     Estados lógicos
+ * ledStatus------GPIO 33---------On=>LOW, Off=>HIGH
+ * ledFlash-------GPIO 4----------On=>HIGH, Off=>LOW
+ * 
+ * SensorMAX30100 ESP32CAM        Resistores
+ * Vin            5V
+ * GND            GND
+ * SDA            14
+ * SCL            15
+ * 
+ */
+
+//Bibliotecas
+#include <WiFi.h>  // Biblioteca para el control de WiFi
+#include <PubSubClient.h> //Biblioteca para conexion MQTT
+#include <Wire.h> //Bilbioteca de comunicacion I2C
+#include "MAX30105.h" //Biblioteca del sensor
+#include "spo2_algorithm.h" //Biblioteca para el calculo de la oxigenacion
+
+//Datos de WiFi
+const char* ssid = "AXTEL XTREMO-18D6";  // Aquí debes poner el nombre de tu red
+const char* password = "038C18D6";  // Aquí debes poner la contraseña de tu red
+
+//Datos del broker MQTT
+const char* mqtt_server = "192.168.15.29"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
+IPAddress server(192,168,15,29);
+
+// Objetos
+WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
+PubSubClient client(espClient); // Este objeto maneja los datos de conexion al broker
+MAX30105 particleSensor; //Objeto para el sensor
+
+//Inicio de variables y constantes del sensor MAX30100
+#define MAX_BRIGHTNESS 255
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint16_t irBuffer[100]; //infrared LED sensor data
+uint16_t redBuffer[100];  //red LED sensor data
+#else
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+#endif
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+byte pulseLED = 12; //Must be on PWM pin
+byte readLED = 13; //Blinks with each data read
+//Fin de variables y constantes del sensor MAX30100
+
+// Variables
+int flashLedPin = 4;  // Para indicar el estatus de conexión
+int statusLedPin = 33; // Para ser controlado por MQTT
+long timeNow, timeLast; // Variables de control de tiempo no bloqueante
+int data = 0; // Contador
+int wait = 5000;  // Indica la espera cada 5 segundos para envío de mensajes MQTT
+
+//Variables de manejo del sensor
+long timeLast2;//Tracker de tiempo de lecturas del sensor
+int waitMax30100 = 1000;//Tiempo entre lecturas del sensor
+
+// Inicialización del programa
+void setup() {
+  // Iniciar comunicación serial
+  Serial.begin (115200);
+  //Configuracion de los leds indicadores de conexion
+  pinMode (flashLedPin, OUTPUT);
+  pinMode (statusLedPin, OUTPUT);
+  digitalWrite (flashLedPin, LOW);
+  digitalWrite (statusLedPin, HIGH);
+  //Configuracion de los leds indicadores del sensor
+  pinMode(pulseLED, OUTPUT);
+  pinMode(readLED, OUTPUT);
+
+  //Inicia conexion a WiFi
+  Serial.println();
+  Serial.println();
+  Serial.print("Conectar a ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password); // Esta es la función que realiz la conexión a WiFi
+ 
+  while (WiFi.status() != WL_CONNECTED) { // Este bucle espera a que se realice la conexión
+    digitalWrite (statusLedPin, HIGH);
+    delay(500); //dado que es de suma importancia esperar a la conexión, debe usarse espera bloqueante
+    digitalWrite (statusLedPin, LOW);
+    Serial.print(".");  // Indicador de progreso
+    delay (5);
+  }
+  
+  // Cuando se haya logrado la conexión, el programa avanzará, por lo tanto, puede informarse lo siguiente
+  Serial.println();
+  Serial.println("WiFi conectado");
+  Serial.println("Direccion IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Si se logro la conexión, encender led
+  if (WiFi.status () > 0){
+  digitalWrite (statusLedPin, LOW);
+  }
+  
+  delay (1000); // Esta espera es solo una formalidad antes de iniciar la comunicación con el broker
+
+  // Conexión con el broker MQTT
+  client.setServer(server, 1883); // Conectarse a la IP del broker en el puerto indicado
+  client.setCallback(callback); // Activar función de CallBack, permite recibir mensajes MQTT y ejecutar funciones a partir de ellos
+  delay(1500);  // Esta espera es preventiva, espera a la conexión para no perder información
+
+  //Conexion con el sensor
+  Wire.begin (14,15);
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    while (1);
+  }
+
+  Serial.println(F("Attach sensor to finger with rubber band. Readings Will Start after 5 seconds"));
+  delay (5000);
+  
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  
+  //---Esta seccion se ejecuta una sola vez al inicio del programa de ejemplo---
+  // Esta seccion permite realizar la obtencion de los primeros datos necesarios para el calculo del HR y SpO2  
+
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+    Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.println(irBuffer[i], DEC);
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+    
+  //Inicio de secuencias no bloqueantes
+  timeLast = millis (); // Inicia el control de tiempo
+}// fin del void setup ()
+
+// Cuerpo del programa, bucle principal
+void loop() {
+  //Verificar siempre que haya conexión al broker
+  if (!client.connected()) {
+    reconnect();  // En caso de que no haya conexión, ejecutar la función de reconexión, definida despues del void setup ()
+  }// fin del if (!client.connected())
+  client.loop(); // Esta función es muy importante, ejecuta de manera no bloqueante las funciones necesarias para la comunicación con el broker
+  
+  timeNow = millis(); // Control de tiempo para esperas no bloqueantes
+
+  //Inicio de lectura del sensor
+  if (timeNow - timeLast2 > waitMax30100) {
+    timeLast2 = timeNow;
+
+    //Aqui va el codigo de lectura del sensor presente en el ejemplo
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = 25; i < 100; i++)
+    {
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
+
+    //take 25 sets of samples before calculating the heart rate.
+    for (byte i = 75; i < 100; i++)
+    {
+      while (particleSensor.available() == false) //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
+
+      digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+     Serial.print(F(", HR="));
+      Serial.print(heartRate, DEC);
+
+      Serial.print(F(", SPO2="));
+      Serial.println(spo2, DEC);
+    }
+
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+    //Fin de codigo del lectura del sensor presente en el ejemplo
+  }
+  //Fin de lectura del sensor  
+
+  //Envio de datos por MQTT
+  if (timeNow - timeLast > wait) { // Manda un mensaje por MQTT cada cinco segundos
+    timeLast = timeNow; // Actualización de seguimiento de tiempo
+
+    //-----Aqui se colocan los datos del sensor a ser enviados-----
+
+    //-----Aqui terminan la configuracion de los datos del sensor a enviar ---
+
+    //Conversion de datos para envio MQTT
+    char dataString[8]; // Define una arreglo de caracteres para enviarlos por MQTT, especifica la longitud del mensaje en 8 caracteres
+    dtostrf(data, 1, 2, dataString);  // Esta es una función nativa de leguaje AVR que convierte un arreglo de caracteres en una variable String
+    Serial.print("Contador: "); // Se imprime en monitor solo para poder visualizar que el evento sucede
+    Serial.println(dataString);
+    client.publish("codigoIoT/SIC/sintmas", dataString); // Esta es la función que envía los datos por MQTT, especifica el tema y el valor
+  }// fin del if (timeNow - timeLast > wait)
+}// fin del void loop ()
+
+// Funciones de usuario
+
+// Esta función permite tomar acciones en caso de que se reciba un mensaje correspondiente a un tema al cual se hará una suscripción
+void callback(char* topic, byte* message, unsigned int length) {
+
+  // Indicar por serial que llegó un mensaje
+  Serial.print("Llegó un mensaje en el tema: ");
+  Serial.print(topic);
+
+  // Concatenar los mensajes recibidos para conformarlos como una varialbe String
+  String messageTemp; // Se declara la variable en la cual se generará el mensaje completo  
+  for (int i = 0; i < length; i++) {  // Se imprime y concatena el mensaje
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+
+  // Se comprueba que el mensaje se haya concatenado correctamente
+  Serial.println();
+  Serial.print ("Mensaje concatenado en una sola variable: ");
+  Serial.println (messageTemp);
+
+  // En esta parte puedes agregar las funciones que requieras para actuar segun lo necesites al recibir un mensaje MQTT
+
+  // Ejemplo, en caso de recibir el mensaje true - false, se cambiará el estado del led soldado en la placa.
+  // El ESP323CAM está suscrito al tema esp/output
+  if (String(topic) == "esp32/output") {  // En caso de recibirse mensaje en el tema esp32/output
+    if(messageTemp == "true"){
+      Serial.println("Led encendido");
+      digitalWrite(flashLedPin, HIGH);
+    }// fin del if (String(topic) == "esp32/output")
+    else if(messageTemp == "false"){
+      Serial.println("Led apagado");
+      digitalWrite(flashLedPin, LOW);
+    }// fin del else if(messageTemp == "false")
+  }// fin del if (String(topic) == "esp32/output")
+}// fin del void callback
+
+// Función para reconectarse
+void reconnect() {
+  // Bucle hasta lograr conexión
+  while (!client.connected()) { // Pregunta si hay conexión
+    Serial.print("Tratando de contectarse...");
+    // Intentar reconexión
+    if (client.connect("ESP32CAMClient")) { //Pregunta por el resultado del intento de conexión
+      Serial.println("Conectado");
+      client.subscribe("esp32/output"); // Esta función realiza la suscripción al tema
+    }// fin del  if (client.connect("ESP32CAMClient"))
+    else {  //en caso de que la conexión no se logre
+      Serial.print("Conexion fallida, Error rc=");
+      Serial.print(client.state()); // Muestra el codigo de error
+      Serial.println(" Volviendo a intentar en 5 segundos");
+      // Espera de 5 segundos bloqueante
+      delay(5000);
+      Serial.println (client.connected ()); // Muestra estatus de conexión
+    }// fin del else
+  }// fin del bucle while (!client.connected())
+}// fin de void reconnect(
